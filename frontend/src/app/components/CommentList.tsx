c2f2813f85fcf6b1f9c2c84a084c4da6aabe34d6
halfaraj <hawra.isa94@gmail.com>
2024-09-09 13:40:40 +0300

    colorized comments

--- frontend/src/app/components/CommentList.tsx
+++ frontend/src/app/components/CommentList.tsx
@@ -1,34 +1,5 @@
-
-// const CommentList = ({ comments }) => {
-//   return (
-//     <div className="card">
-//       <div className="card-body">
-//         <h5 className="card-title">Comments</h5>
-//         {comments.map((comment, index) => (
-//           <div key={index} className="mb-3">
-//             <div className="d-flex align-items-start">
-//               <img
-//                 src={"placeholder.jpg"}
-//                 alt={comment.author.name}
-//                 width={40}
-//                 height={40}
-//                 className="rounded-circle me-2"
-//               />
-//               <div>
-//                 <h6 className="mb-0">{comment.author.name}</h6>
-//                 <small className="text-muted">{comment.time}</small>
-//                 <p className="mt-1 mb-0">{comment.content}</p>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CommentList;
 import Comment from './Comment';
+import { randomColor } from "../components/colors"; 
 
 const CommentList = ({ comments }) => {
   return (
@@ -36,7 +7,9 @@ const CommentList = ({ comments }) => {
       <h5 className="mb-4">Comments</h5>
       {comments.map((comment, index) => (
         <div key={index} className="card mb-3">
-          <div className="card-body">
+          <div className="card-body" style={{
+            backgroundColor: randomColor(), // Set the background color to a random color
+                }}>
             <Comment
               author={comment.author}
               content={comment.content}
