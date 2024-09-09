1596d0d8a3c5964bad9770d52e2d269104270735
halfaraj <hawra.isa94@gmail.com>
2024-09-09 13:12:09 +0300

    UI enhancements

--- frontend/src/app/components/PostContent.tsx
+++ frontend/src/app/components/PostContent.tsx
@@ -2,31 +2,42 @@ import Image from 'next/image';
 
 const PostContent = ({ content, images, avatar, name, time }) => {
     return (
-      <div className="post-card">
-        <div className="postcard-user">
+      <div className="card mb-4 shadow-sm">
+        {/* Author Info */}
+        <div className="card-header d-flex align-items-center">
+          <Image
+            src={"/placeholder.jpg"}
+            alt="Author's Avatar"
+            width={50}
+            height={50}
+            className="rounded-circle me-3"
+          />
+          <div>
+            <h5 className="mb-0">{name}</h5>
+            <small className="text-muted">{time}</small>
+          </div>
+        </div>
 
-      <div className="user-details">
-      <Image src={avatar} alt="Author's Avatar" width={50} height={50} className="w-10 h-10 rounded-full"/> &nbsp;{name}
-        <p style={{ fontFamily: 'Roboto, sans-serif', fontSize: '10px', marginLeft:'10px' }}>{time}</p>
+        {/* Post Content */}
+        <div className="card-body">
+          <p className="card-text">{content}</p>
+
+          {/* Post Image */}
+          {images && images.length > 0 && (
+            <div className="mb-4">
+              <Image
+                src={"/placeholder.jpg"} // Assuming only one image is used
+                alt="Post Image"
+                width={600}
+                height={300}
+                className="img-fluid rounded"
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-    <div className="card-content">
-      <p>{content}</p>
-    </div>
-    <div className="liked-bar">
-      {/* <p><i className="fa-solid fa-heart"></i>liked by Sara Jameel & others</p> */}
-    </div>
-    <div className="react-bar">
-      <button className="react-button"><i className="fa-solid fa-heart"></i>Like</button><br></br><br></br>
-      <div className="w-100 p-2">
-        <input type="text" className="form-control" name="comment" placeholder="Comment" />
-        <button className="react-button"><i className="fa-solid fa-comment"></i> Comment</button>
-    </div>
-      {/* <button className="react-button"><i className="fa-solid fa-paper-plane"></i> Share</button> */}
-    </div>
-  </div>
     );
-  };
-  
-  export default PostContent;
-  
+};
+
+export default PostContent;
+
