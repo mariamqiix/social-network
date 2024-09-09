3fef9a84e7434f3ba3f25fb0260b7b2cea110d37
halfaraj <hawra.isa94@gmail.com>
2024-09-09 13:11:12 +0300

    deUI ehancements

--- frontend/src/app/components/Comment.tsx
+++ frontend/src/app/components/Comment.tsx
@@ -1,21 +1,18 @@
-import ReplyButton from './NormalBtn';
-
-const Comment = ({ author, content, time, likes }) => {
+const Comment = ({ author, content, time }) => {
   return (
-    <div className="card-comments">
-      <div className="user-comment">
-        <img src={author.avatar} alt="Avatar" className="w-10 h-10 rounded-full" style={{ width: '40px', height: '40px' }}/>&nbsp;{author.name}
-
-        <div className="ml-3">
-          {/* <h3 className="text-sm font-semibold">{author.name}</h3> */}
-          <p className="text-xs text-gray-500" style={{ fontSize: '10px' }}>{time}</p>
-        </div>
+    <div className="d-flex align-items-start">
+      <img
+        src={"placeholder.jpg"}
+        alt="Avatar"
+        className="rounded-circle"
+        style={{ width: '40px', height: '40px', marginRight: '10px' }}
+      />
+      <div>
+        <h6 className="mb-1">{author.name}</h6>
+        <small className="text-muted">{time}</small>
+        <p className="mt-2 mb-1">{content}</p>
       </div>
-      <p className="mt-2">{content}</p>
-        <br></br><hr></hr>
-        {/* <ReplyButton /> */}
-      </div>
-
+    </div>
   );
 };
 
