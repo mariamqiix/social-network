65bc300ccd57f687c16d571fd0222391322735a9
halfaraj <hawra.isa94@gmail.com>
2024-09-09 13:30:05 +0300

    design enhancement + fontAwesome used for icons

--- frontend/src/app/components/PostActions.tsx
+++ frontend/src/app/components/PostActions.tsx
@@ -1,12 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faCommentAlt, faShare } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+
 const PostActions = ({ likes }) => {
-    return (
-      <div className="mt-4 flex justify-between items-center text-gray-500 text-sm">
-        <div className="flex space-x-2">
-          <span>❤️ {likes}</span>
-        </div>
+  return (
+    <div className="mt-4 d-flex justify-content-between align-items-center text-muted">
+      {/* Show the number of likes */}
+      {/* <div>
+        <span>
+          <FontAwesomeIcon icon={faThumbsUp} /> {likes}
+        </span>
+      </div> */}
+      
+      {/* Action buttons with icons */}
+      <div>
+        <button className="btn btn-outline-primary btn-sm me-2">
+          <FontAwesomeIcon icon={faThumbsUp} /> Like
+        </button>
+        <button className="btn btn-outline-success btn-sm me-2">
+          <FontAwesomeIcon icon={faCommentAlt} /> Comment
+        </button>
+        <button className="btn btn-outline-secondary btn-sm">
+          <FontAwesomeIcon icon={faShare} /> Share
+        </button>
       </div>
-    );
-  };
-  
-  export default PostActions;
-  
+    </div>
+  );
+};
+
+export default PostActions;
