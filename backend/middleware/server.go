eb3c7335892d047d13a7088915c589af2fe680fb
mariamqiix <mariamabbas00625@gmail.com>
2024-09-10 06:48:01 +0000

    nothing new

--- backend/middleware/server.go
+++ backend/middleware/server.go
@@ -1,1 +1,17 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GoLive(){
+
+	http.HandleFunc("/homePage",HomePageHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {}
