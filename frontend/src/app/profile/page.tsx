39d01e2542e1d53fb78b393e3ba6a6c1b07b98b6
rhelal <ruqayahhelal@gmail.com.com>
2024-10-26 21:52:35 +0300

    profileStuff/incomplete

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,37 +1,139 @@
 'use client';
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/selectors";
+import "../../../public/profilePage.css";
 import Image from "next/image";
 import Card from "../components/card";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAt, faCalendarDay, faEnvelope, faSignature } from "@fortawesome/free-solid-svg-icons";
 import { useEffect } from "react";
 
+
+// import React, { useRef, useState, useEffect } from 'react';
+
+import { useState } from "react";
+
+
 export default function page() {
     const user = useSelector(selectUser);
     useEffect(() => {
-        fetch("http://localhost:8080/user/profile/", { credentials: 'include' }).then((res) => {
+        fetch("http://localhost:8080/user/profile/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({ user_id: -1 })
+        }).then((res) => {
             res.text().then((data) => {
                 console.log(data);
             });
         });
-    }, [fetch]);
+    }, []);
     if (user) {
-        return <Card title={user.username}>
-            <Image
-                className="rounded-circle"
-                //src={user.image ?? "/placeholder.jpg"}
-                src={"/placeholder.jpg"}
-                width={80}
-                height={80}
-                alt="Avatar"
-            />
-            <h2><FontAwesomeIcon icon={faAt} />{user.username}</h2>
-            <h2>{user.firstName} {user.lastName}</h2>
-            <p><FontAwesomeIcon icon={faSignature} /> {user.nickname}</p>
-            <p><FontAwesomeIcon icon={faCalendarDay} /> {user.dob.split("T")[0]}</p>
-            <p><FontAwesomeIcon icon={faEnvelope} /> {user.email}</p>
-        </Card>;
+
+        const changeProfileContent = (type: String) => {
+            // Function to be executed on button click
+            const profileContent = document.getElementById("profileContent");
+
+            // const url = new URL("http://localhost:8080/user/profile/");
+            // fetch(url, {
+            //     method: "GET",
+            // })
+            //     .then((response) => {
+            //         if (!response.ok) {
+            //             //i should handle bad response
+            //         }
+            //         return response.json();
+            //     })
+            //     .then((data) => {
+            //         if (data && data.Posts) {
+            //             displayPostOnProfile(data.Posts, type);
+            //         } else {
+            //             console.error("Invalid data format. Expected profileView with Posts.");
+            //         }
+            //     })
+            //     .catch((error) => {
+            //         console.error("Error fetching profile:", error);
+            //     });
+        };
+
+        const age = calculateAge(user.dob)
+        return (
+            <div className="container">
+                <div className="userPic" id="userPic" style={{ backgroundImage: `data:image/jpeg;base64,${user.image_url}` }}></div>
+
+                <div className="profileUsername" id="profileUsername">{user.firstName} {user.lastName} ({user.username})</div>
+                
+                <div className="userInfo" id="userInfo">
+                    <table className= "userInfo">
+                        <tr>
+                            <th>Bio: {user.bio}</th>
+                        </tr>
+                        <tr>
+                            <th>Age: {age}</th>
+                        </tr>
+                        <tr>
+                            <th>Email: {user.email}</th>
+                        </tr>
+                    </table>
+                </div>
+                <div className="userBarDiv">
+                <table className= "userBar">
+                    <tr>
+                        <th id="PostsTh" onClick={() => changeProfileContent("Posts")}>
+                            Posts
+                        </th>
+                        <th id="likesTh" onClick={() => changeProfileContent("Likes")}>
+                            Likes
+                        </th>
+                        <th id="dislikesTh" onClick={() => changeProfileContent("DisLikes")}>
+                            Dislikes
+                        </th>
+                    </tr>
+                </table>
+
+                <div id="profileContent">
+                    <p id="selectedColumn">Click on a column to change the content.</p>
+                </div>
+                </div>
+            </div>);
+
     }
+
     return <h1>You need to login</h1>;
+}
+
+function calculateAge(birthDateString: string) {
+    const birthDate = new Date(birthDateString);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    // If the birth date hasn't occurred yet this year, subtract one year from the age.
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+function displayPostOnProfile(Posts: any, type: string) {
+    if (Array.isArray(Posts)) {
+        createPost(Posts, type);
+    } else {
+        console.error("Invalid data format. Expected an array of posts.");
+    }
+}
+
+function createPost(Posts: any, caseString: string) {
+    const [navigationContent, setNavigationContent] = useState('');
+
+    const clearNavigationContent = () => {
+        setNavigationContent('');
+    };
+
+    Posts.forEach((post: any) => {
+
+    })
 }
