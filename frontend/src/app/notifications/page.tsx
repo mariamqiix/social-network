6ec67345b1d6b73c18bf2c9c7bfee75ecbe1f0fe
HassanAliKadhem <hassanali92.ha@gmail.com>
2024-11-04 13:44:43 +0300

    post with image started

--- frontend/src/app/notifications/page.tsx
+++ frontend/src/app/notifications/page.tsx
@@ -18,31 +18,35 @@ export default function page() {
     const dispatch = useDispatch();
     useEffect(() => {
         fetch("http://localhost:8080/user/notifications/", { credentials: 'include' }).then((res) => {
-            res.json().then((data) => {
-                console.log(data);
-                if (data) {
-                    data.forEach((element: any) => {
-                        dispatch(addNotification({ id: element.id, type: "message", title: element.type, message: element.message, link: "", showToast: false, extraData: element.group_id }));
-                    });
-                }
-            });
+            if (res.status == 200) {
+
+                res.json().then((data) => {
+                    console.log(data);
+                    if (data) {
+                        data.forEach((element: any) => {
+                            dispatch(addNotification({ id: element.id, type: "message", title: element.type, message: element.message, link: "", showToast: false, extraData: element.group_id }));
+                        });
+                    }
+                });
+            } else {
+                res.text().then(text => {
+                    console.error(text);
+                })
+            }
         });
     }, [fetch]);
 
     if (notifications.length > 0) {
-
         return notifications.map(notification => <Card key={notification.id} title={notification.title} color={notificationColors[notification.type]}>
             <p>{notification.message}</p>
-            <button className="btn btn-primary" onClick={() => {
-                if (notification.title == "GroupInvite") {
-                    fetch("http://localhost:8080/user/responds/groupInviteResponse", { method: "POST", credentials: 'include', body: JSON.stringify({ group_id: notification.extraData, response: "Accept" }) }).then((res) => {
-                        console.log(res.status);
-                        res.text().then((data) => {
-                            console.log(data);
-                        });
+            {notification.title == "GroupInvite" ? <button className="btn btn-primary" onClick={() => {
+                fetch("http://localhost:8080/user/responds/groupInviteResponse", { method: "POST", credentials: 'include', body: JSON.stringify({ group_id: notification.extraData, response: "Accept" }) }).then((res) => {
+                    console.log(res.status);
+                    res.text().then((data) => {
+                        console.log(data);
                     });
-                }
-            }}>Accept</button>
+                });
+            }}>Accept</button> : <div></div>}
         </Card>);
     } else {
         return <p>You have no notifications</p>
